735984f3bde6e6dd4662dae8f06ad1652d945e6b
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-09 02:08:02 +0000

    Updates

---
+++ precache-manifest.277e806e44992c1d268011fec6d098c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11d30dd391a5bce6b98ede99e03a25e",
+    "url": "/ontrack-test/index.html"
+  },
+  {
+    "revision": "df2a3f5a41b22d3f6f67",
+    "url": "/ontrack-test/static/css/main.dd644f71.chunk.css"
+  },
+  {
+    "revision": "9e8ad3fc3db8fe35b67e",
+    "url": "/ontrack-test/static/js/2.d0b25d51.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/ontrack-test/static/js/2.d0b25d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2a3f5a41b22d3f6f67",
+    "url": "/ontrack-test/static/js/main.922dfc89.chunk.js"
+  },
+  {
+    "revision": "e3f54cb6e52d1c082fad",
+    "url": "/ontrack-test/static/js/runtime-main.8fa452cd.js"
+  }
+]);
